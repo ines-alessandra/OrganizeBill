f2437587ecfc173bb4d9b5b082287470a9b917c1
Iasmin <iasminraquelsou@gmail.com>
2024-02-19 05:04:00 -0300

    view da tela inicial, ajuster navbar

--- organize-bill-front/app/page.js
+++ organize-bill-front/app/page.js
@@ -3,9 +3,12 @@ import Login from "@/pages/components/Login";
 
 export default function Home() {
   return (
-    <div className="flex flex- h-screen justify-center items-center gap-10" >
-      <h1 className="text-2xl font-black" >OrganizeBill</h1>
-      <Login />
+    <div className="flex h-screen place-items-center max-w-7xl m-auto gap-14" >
+      <div className="w-2/4">
+        <h1 className="text-2xl font-black mb-2" >OrganizeBill</h1>
+        <p>O projeto de gerenciamento financeiro proposto visa oferecer um sistema que seja possível cadastrar, gerenciar, estabelecer metas financeiras. Este projeto foi desenvolvido como parte da avaliação parcial da disciplina de Banco de Dados</p>
+      </div>
+      <Login/>
     </div>
   );
 }
